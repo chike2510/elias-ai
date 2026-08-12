e3da48843f873b0f084118206ac4fdf6378acdd5
chike2510 <onovochikeziri84@gmail.com>
2026-08-12 10:30:40 +0100

    Delete page.tsx

--- app/projects/storeos/code/page.tsx
+++
@@ -1,3 +0,0 @@
-import CodeWorkbenchScreen from "@/components/screens/CodeWorkbenchScreen";
-import { ProjectStoreProvider } from "@/components/store/ProjectStore";
-export default function Page() { return <ProjectStoreProvider><CodeWorkbenchScreen /></ProjectStoreProvider>; }
