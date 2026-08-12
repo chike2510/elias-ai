35a73a19c287d81625c44fa45b32c6c2fdbfc460
chike2510 <onovochikeziri84@gmail.com>
2026-08-12 11:12:27 +0100

    Delete ModalSheet.tsx

--- components/ModalSheet.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-export default function ModalSheet({children, onClose}: {children: React.ReactNode; onClose: () => void}) {
-  return <div className="sheet-backdrop" onClick={onClose}>
-    <section className="action-sheet drawer" onClick={(e) => e.stopPropagation()}>{children}</section>
-  </div>
-}
