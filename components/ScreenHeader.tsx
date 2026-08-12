fd161fc63e2932c7d59b88c2f48c7b84d9da197c
chike2510 <onovochikeziri84@gmail.com>
2026-08-12 11:12:35 +0100

    Delete ScreenHeader.tsx

--- components/ScreenHeader.tsx
+++
@@ -1,5 +0,0 @@
-import Link from "next/link";
-import { ChevronLeft } from "lucide-react";
-export default function ScreenHeader({title, back="/"}:{title:string; back?:string}) {
-  return <div className="screen-title"><Link href={back} className="back"><ChevronLeft size={21}/></Link><span>{title}</span></div>;
-}
